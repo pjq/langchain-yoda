383638e6549800bf55b8c0fd3b9d0f0f11531f2a
Jianqing Peng <pengjianqing@gmail.com>
2023-05-12 00:21:17 +0800

    refactor the yoda.py

--- yoda.py
+++ yoda.py
@@ -1,66 +1,69 @@
 import os
-from langchain.agents import Tool
+from langchain.agents import Tool, initialize_agent, AgentType
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import initialize_agent, AgentType
 import lib.utils as proxy
 
-# Set up Google search and other tools
-# search = proxy.createGoogleSearch()
-search = proxy.createGoogleSerper()
-bash = proxy.createBash()
-worlfram= proxy.createWolfram()
-human = proxy.createHuman()
-weather = proxy.createOpenWeatherMap()
-visionCaption = proxy.createVisionCaption()
-visionControl = proxy.createVisionControl()
-tools = [
-    Tool(
-        name="Google Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events or the current state of the world. the input to this should be a single search term."
-    ),
-    Tool(
-        name="Wolfram",
-        func=worlfram.run,
-        description=""
-    ),
-    Tool(
-        name="Human",
-        func=human.run,
-        description=""
-    ),
-    Tool(
-        name="weather",
-        func=weather.run,
-        description="weather information"
-    ),
-    visionCaption,
-    visionControl,
-    # Tool(
-    #     name="vision caption",
-    #     func=weather.run,
-    #     description="vision caption "
-    # ),
-    Tool(
-        name="Bash Process",
-        func=bash.run,
-        description="useful for when you need to control or get the information from the current system"
-    )
-]
+class ChatBot:
+    def __init__(self):
+        # Set up tools
+        search = proxy.createGoogleSerper()
+        bash = proxy.createBash()
+        wolfram = proxy.createWolfram()
+        human = proxy.createHuman()
+        weather = proxy.createOpenWeatherMap()
+        vision_caption = proxy.createVisionCaption()
+        vision_control = proxy.createVisionControl()
 
-# Set up the chatbot
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-llm = ChatOpenAI(temperature=0)
-agent_chain = initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
+        self.tools = [
+            Tool(
+                name="Google Search",
+                func=search.run,
+                description="Useful for answering questions about current events or the current state of the world. The input should be a single search term."
+            ),
+            Tool(
+                name="Wolfram",
+                func=wolfram.run,
+                description="Computational knowledge engine."
+            ),
+            Tool(
+                name="Human",
+                func=human.run,
+                description="Human agent for difficult questions."
+            ),
+            Tool(
+                name="Weather",
+                func=weather.run,
+                description="Provides weather information."
+            ),
+            vision_caption,
+            vision_control,
+            Tool(
+                name="Bash Process",
+                func=bash.run,
+                description="Useful for controlling or getting information from the current system."
+            )
+        ]
 
-# Start chatting with the user
-print('Bot: Hello! How can I assist you today?')
-while True:
-    user_input = input('You: ')
-    if user_input.lower() in ['exit', 'bye', 'quit']:
-        print('Bot: Goodbye!')
-        break
-    response = agent_chain.run(user_input)
-    # print(f'Bot: {response["action_input"]}')
-    print(f'Bot: {response}')
+        # Set up the chatbot
+        self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        self.llm = ChatOpenAI(temperature=0)
+        self.agent_chain = initialize_agent(self.tools, self.llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=self.memory)
+
+    def start_chat(self):
+        # Start chatting with the user
+        print('Bot: Hello! How can I assist you today?')
+        while True:
+            try:
+                user_input = input('You: ')
+                if user_input.lower() in ['exit', 'bye', 'quit']:
+                    print('Bot: Goodbye!')
+                    break
+                response = self.agent_chain.run(user_input)
+                print(f'Bot: {response}')
+            except Exception as e:
+                print(f'An error occurred: {e}')
+
+if __name__ == '__main__':
+    bot = ChatBot()
+    bot.start_chat()
